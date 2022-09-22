20abcfd701d3e26c22b604f1d3413798007470b8
anointingjoe <josephodey.88@hmail.com>
2022-09-22 11:38:21 +0000

    correct 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-import sys
-
-if __name__ != "__main__":
-    exit()
-
-i = 0
-result = 0
-for argument in sys.argv:
-    if i != 0:
-        result += int(argument)
-    else:
-        i += 1
-print("{:d}".format(result))
+if __name__ == "__main__":
+    from sys import argv
+    add = 0
+    for s in argv[1:]:
+        add += int(s)
+    print("{:d}".format(add))
